d84a6a7d779757e7c7581e4c9bbd12b482d4ac97
Marcos-RN <marcosro1999@gmail.com>
2021-10-26 16:14:08 +0200

    Prueba

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -4,4 +4,29 @@ object HelloWorld {
     print("Hello World Scala")
   }
 
+  def prefijoComun(n1: Int, n2: Int): Int = {
+    require(n1>0 && n2>0)
+    var x = n1
+    var y = n2
+    var z = 0
+    // Punto A
+    while(x!=y) {
+      // Punto B
+      z = z+1
+      // Punto C
+      if(x>y) {
+        // Punto D
+        x = x/10
+      } else {
+        //Punto E
+        y = y/10
+      }
+      // Punto F
+    }
+    // Punto G
+     x
+  }
+
+  print(prefijoComun(32845,32892934))
+
 }
